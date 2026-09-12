7915cde942be3dc1abf527a27e7301763b585971
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-12 23:14:14 +0530

    Add files via upload

---
+++ python_crasch_course_chapter_2-practice.py
@@ -0,0 +1,64 @@
+"hallo python world"
+print("hallo python world")
+message = "hallo python crash course"
+print(message)
+name = "anshul chaudhary"
+print(name)
+name = "anshul chaudhary"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = "anshul"
+last_name = "chaudhary"
+full_name = f"{first_name} {last_name}"
+message = f"Hallo,{full_name.title()}"
+print(message)
+message = "i m not intrested in your way now i m drive the car you put on your side and take peg from your bottle"
+message_2 = "how are you where are you now days what are you donig what do you have dn"
+full_message = f"{message} {message_2}"
+replay_me = f"hallo,{full_message.title()}"
+print(replay_me)
+print(replay_me.lower())
+print(replay_me.upper())
+print(replay_me.title())
+first_name = "vaibhav"
+last_name = "chaudhary"
+full_nmae = f"{first_name} {last_name}"
+message = f"Hallo,{full_name.title()}"
+print(message)
+#adding_white_space_to_strings_with_Tab_or_Newlines
+#to add a tab to your text , use the charecter combinatin:
+print("python")
+print("\tpython")
+print("language:\nPython\nC\nJavaScript")
+#practice newlines(\n) and Tabs(\t)
+print("python")
+print("\nPyhton")
+print("language;\nAnshul\nVaibhav\nDeepak")
+print( "language:\n\tAnshul\n\tVaibhav\n\tDeepak")
+# one more time practice
+print("anshul")
+print("\nAnshul")
+print("Friends:\nAnshulchoudhary\nVaibhavchaudhary\nDeepakdhariwal")
+print("Friend:\n\tAnshulchaudhary\n\tVaibhavchaudhary\n\tDeepakdhariwal")
+#striping -whitespace
+favorite_language="python "
+print(favorite_language.rstrip())
+favorite_language = "python"
+favorite_language = favorite_language.upper()
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.title())
+print(favorite_language.lstrip())
+print(favorite_language.strip().title())
+#Removing_Prefixes
+nostarch_URL = "http://nostarch.com"
+simple_URL = nostarch_URL.removeprefix('http://')
+print(simple_URL)
+print(simple_URL.title())
+first_name = "anshul"
+last_name = "chaudhary"
+full_name = f"{first_name} {last_name}"
+message = f"Hallo,{full_name.title()}"
+print(message)
+
