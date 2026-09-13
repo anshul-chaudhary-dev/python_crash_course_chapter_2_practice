10132b91c16fbf960a63b9cbee97eaa30ae68ea0
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-13 17:25:56 +0530

    Add files via upload

---
+++ number_exercise_by_book.py
@@ -0,0 +1,17 @@
+universe_age = 14_000_000_000
+print(universe_age)
+x,y,z, = 0,1,2
+print(x)
+print(y)
+print(z)
+
+
+print(3+5)
+print(4*2)
+print(10-2)
+print(int(16/2))
+
+
+
+favorite_number = "53"
+print(f"favorite number is {favorite_number}")
