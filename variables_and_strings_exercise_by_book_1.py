cc34ecffc72263cb66067b2a128cade4a4dc2e92
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-13 16:42:23 +0530

    Add files via upload

---
+++ variables_and_strings_exercise_by_book_1.py
@@ -0,0 +1,33 @@
+name = "eric"
+print(f"Hallo,{name}.how are you today")
+
+
+
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+
+name_person ="Albert Eintein"
+qoute = '"A Person who never made a mistake never tried anithing new"'
+print(f"{name_person} once said, {qoute}")
+
+
+
+famous_person = "Albert Eintein"
+qoute = '"a person who never made a mistake never tried anithing new"'
+message = f"{famous_person} once said,{qoute}"
+print(message)
+
+
+
+
+name_space = " Eric    "
+print(name_space.lstrip())
+print(name_space.rstrip())
+print(name_space.strip())
+
+
+
