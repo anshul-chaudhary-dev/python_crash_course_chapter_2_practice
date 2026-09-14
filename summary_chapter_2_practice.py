b60c23e2c6ab85a843fe3214929edf70ae6eaea7
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-14 22:42:14 +0530

    Add files via upload

---
+++ summary_chapter_2_practice.py
@@ -0,0 +1,8 @@
+Summary
+in this cahpter you learned how to work with variables.
+you learned to use discriptive variableds name and resolves names error and ssyntex error when they arise.
+you learned what string are and how to display them lower case, upper case and title case.
+you atarted using whitespaces to organizeoutput neatly  and you leaarn how to remove unneede eliments from hte string.
+you starting working with integer and floats,nad you learned some of the ways you can work with numerical data.
+you also learn to write expanetry comments to make your code esiar for you and uther to read.
+Finally you read about the philosophy of keeping your code as simple as possible , whenever possible.
